652fb7c4ba35567121e8e10008326ebec8f502c4
fahidroid <fahidroid@gmail.com>
2017-04-13 19:05:18 +0530

    deleted node modules

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express')
 var app = express()
+var port = process.env.PORT || 5678;
 app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.send(__dirname+'/public/index.html');
 })
 
-app.listen(5000)
+app.listen(port);
